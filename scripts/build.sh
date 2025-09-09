654261928070b0c0ce484bbeef5f86fb5aef3deb
Vladimir Zaikin <friend.zva@yandex.ru>
2025-09-10 00:24:32 +0300

    fix: OPENCL_SUPPORT in build script
    Signed-off-by: Vladimir Zaikin <friend.zva@yandex.ru>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 BASEDIR=$(realpath "$(dirname "$0")")
 ROOTDIR=$(realpath "$BASEDIR/..")
 
-cmake -S . -B "$ROOTDIR/build" -DUNIT_TESTING=ON -DOPENCL_SUPPORT=ON
+cmake -S . -B "$ROOTDIR/build" -DUNIT_TESTING=ON -DOPENCL_SUPPORT=OFF
 cmake --build "$ROOTDIR/build"
